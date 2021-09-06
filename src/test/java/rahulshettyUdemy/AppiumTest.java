c7df4a9c262a450ccce7cc07b7c9bd870634638b
Saumya <singhsaumyait034@gmail.com>
2021-09-06 10:54:34 +0530

    Maven Java initial commit

---
+++ src/test/java/rahulshettyUdemy/AppiumTest.java
@@ -0,0 +1,19 @@
+package rahulshettyUdemy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void WindowsApp()
+	{
+       System.out.println("WindowsApp");
+	}
+	
+	@Test
+	public void IOSApp()
+	{
+		System.out.println("IOSApp");
+	}
+
+}
---
+++ src/test/java/rahulshettyUdemy/RestAssuredTest.java
@@ -0,0 +1,19 @@
+package rahulshettyUdemy;
+
+import org.testng.annotations.Test;
+
+public class RestAssuredTest {
+	
+	@Test
+	public void Rest()
+	{
+		System.out.println("Rest Assured Test");
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+
+}
